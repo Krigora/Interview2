f132d8fdbcede4d9999937a44c4dc61b13273e1b
krigora <ihor.kryvynskyy@icloud.com>
2021-10-13 16:52:19 -0500

    updating Same letters

--- src/JavaCoding/SameLetters.java
+++ src/JavaCoding/SameLetters.java
@@ -1,4 +1,32 @@
 package JavaCoding;
 
+import java.util.Arrays;
+
 public class SameLetters {
-}
+
+    /*
+    •	Write a return method that check if a string is build out of the same letters as another string.
+•	Ex:  same("abc",  "cab"); -> true
+•	same("abc",  "abb"); -> false:
+
+     */
+
+        public static void main(String[] args) {
+            System.out.println(sameLetter());
+        }
+
+        public static boolean sameLetter(){
+            String first = "abc";
+            String second = "cab";
+
+            char[] firstArr = first.toCharArray();
+            char[] secondArr = second.toCharArray();
+
+            Arrays.sort(firstArr);
+            Arrays.sort(secondArr);
+            return Arrays.equals(firstArr,secondArr);
+        }
+
+    }
+
+
