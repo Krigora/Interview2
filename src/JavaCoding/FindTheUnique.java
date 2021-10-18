dfb53446601b03b0e5d876043112150f93fea425
krigora <ihor.kryvynskyy@icloud.com>
2021-10-18 16:36:52 -0500

    creating String - Find the unique

---
+++ src/JavaCoding/FindTheUnique.java
@@ -0,0 +1,39 @@
+package JavaCoding;
+
+public class FindTheUnique {
+
+    /*
+    String - Find the unique
+   	Write a return  method that can find the unique characters from the String
+	Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+
+     */
+
+    public static void main(String[] args) {
+        System.out.println(unique("AAABBBCCCDEF"));
+
+    }
+
+    public static String unique (String letter){
+        String word = "AAABBBCCCDEF";
+
+        String unique = "";
+        for (int i =  0; i < letter.length(); i++){
+            int count = 0;
+
+            for (int j = 0; j < letter.length(); j++){
+                if (letter.charAt(i) == letter.charAt(j)){
+                    count++;
+                }
+            }
+
+            if (count == 1){
+                unique += letter.charAt(i);
+            }
+        }
+        return unique;
+    }
+
+
+
+}
