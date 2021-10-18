98264d469a437aed94140181ef3da6a1d5b85307
krigora <ihor.kryvynskyy@icloud.com>
2021-10-18 16:21:30 -0500

    updating Remove duplicates

---
+++ src/JavaCoding/RemoveDuplicates.java
@@ -0,0 +1,36 @@
+package JavaCoding;
+
+public class RemoveDuplicates {
+
+    /*
+   Write a return method that can remove the duplicated values from String
+Ex:  removeDup("AAABBBCCC")  ==> ABC
+
+    */
+    public static void main(String[] args) {
+        System.out.println(removeDuplicate("AAABBBCCC" ));
+    }
+    public static String removeDuplicate (String word){
+
+        String str = "AAABBBCCC";
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++){
+            if (!unique.contains("" + str.charAt(i))){
+                unique += str.charAt(i);
+            }
+        }
+
+        return unique;
+// updating
+
+    }
+
+
+}
+
+
+
+
+
+
