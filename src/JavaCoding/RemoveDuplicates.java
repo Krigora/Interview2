45c0479d2451d3940e9c5b6cc179ed85bf88585d
krigora <ihor.kryvynskyy@icloud.com>
2021-11-20 13:29:15 -0600

    updating FindTheUnique

--- src/JavaCoding/RemoveDuplicates.java
+++ src/JavaCoding/RemoveDuplicates.java
@@ -4,7 +4,7 @@ public class RemoveDuplicates {
 
     /*
    Write a return method that can remove the duplicated values from String
-Ex:  removeDup("AAABBBCCC")  ==> ABC
+Ex: removeDup("AAABBBCCC") ==> ABC
 
     */
     public static void main(String[] args) {
